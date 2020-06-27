83bd2b70ca5977b0982a3557ed82948facdfe7b6
randyshao <shao.randy@gmail.com>
2020-06-26 23:07:47 -0400

    Reformatted side drawer and color change

---
+++ .history/src/components/Layout/NavBar/SideDrawer/SideDrawer_20200626225303.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Logo from '../../../Logo/Logo';
+import NavLinks from '../NavLinks/NavLinks';
+import '../../../../App.css';
+
+const SideDrawer = (props) => {
+    return (
+        <div className='SideDrawer' style={{marginBottom: 32px}}>
+            <Logo height='11%' />
+            <nav>
+                <NavLinks />
+            </nav>
+        </div>
+    )
+};
+
+export default SideDrawer;

924664bab6e977da04c39aa5311d83f0b3ca9026
Randy Shao <shao.randy@gmail.com>
2020-07-28 18:22:09 -0400

    Updated event banner

--- src/components/Layout/Events/UpcomingEvent.js
+++ src/components/Layout/Events/UpcomingEvent.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import EventBanner from '../../../assets/images/currentevent.JPG'
-import ButtonPill from '../../UI/ButtonPill'
+import EventBanner from '../../../assets/images/currentevent.png';
+import ButtonPill from '../../UI/ButtonPill';
 
 const UpcomingEvent = () => {
-    return (
-        <div className='UpcomingEvent'>
-            <h1>Our Upcoming Event!</h1>
-            <div className='EventBlock'>
-                <div>
-                    <h1>Resume Review Workshop</h1>
-                    <h2></h2>
-                    <h3>Saturday July 25th, 4-5PM PST</h3>
-                    <ButtonPill link='https://zmurl.com/pb-pm-resume-review'>Click here to register</ButtonPill>
-                </div>
-                <div>
-                    <img src={EventBanner} />
-                </div>
-            </div>
+  return (
+    <div className='UpcomingEvent'>
+      <h1>Our Upcoming Event!</h1>
+      <div className='EventBlock'>
+        <div>
+          <h1>Case Studies Workshop</h1>
+          <h2></h2>
+          <h3>Saturday August 1st, 11AM PST</h3>
+          <ButtonPill link='https://zmurl.com/case-studies'>
+            Click here to register
+          </ButtonPill>
         </div>
-    )
-}
+        <div>
+          <img style={{ paddingLeft: '40px' }} src={EventBanner} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default UpcomingEvent;
